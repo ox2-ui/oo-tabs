5fb72786afa5829740b0a9d77043bb3819c64486
Lauricio Su <me@lauricio.com>
2015-10-04 19:36:43 +0700

    updated less dependency to 2.5.0

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'ox2:tabs',
-  summary: 'Tab components',
-  version: '1.0.0',
+  summary: 'TESTING_DO_NOT_USE Tab components',
+  version: '1.1.0',
   git: ' /* Fill me in! */ '
 });
 
@@ -10,16 +10,17 @@ var C = 'client';
 var CS = [C, S];
 
 Package.onUse(function(api) {
-  api.versionsFrom('1.0.2.1');
+  api.versionsFrom('1.2.0.2');
   // Core
   api.use([
     'templating'
     ]);
   // 3rd party
   api.use([
-    'lauricio:less-autoprefixer@1.0.7','mquandalle:jade@0.4.1'
+    'lauricio:less-autoprefixer@2.5.0_3','mquandalle:jade@0.4.1'
     ]);
   api.addFiles('lib/oo-color-variables.import.less', C);
+  api.addFiles('lib/oo-font-variables.import.less', C);
   api.addFiles('lib/oo-tabs.jade', C);
   api.addFiles('lib/oo-tabs.js', C);
   api.addFiles('lib/oo-tabs.less', C);
